de9da36a39141cd69037811b6cdc813b2a5e5198
Alex Martin <alextravc.342@gmail.com>
2022-09-23 08:55:53 -0500

    [ADD]: property list design added

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,17 @@
-import { Header, Navbar } from '../../components';
+import { Featured, Header, Navbar, PropertyList } from '../../components';
+import './home.css';
 
 export const HomePage = () => {
   return (
     <div>
       <Navbar />
       <Header />
+
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+      </div>
     </div>
   );
 };
